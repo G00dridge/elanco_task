97c597a067039d982214893c88059f6bc304c457
G00dridge <up2019073@myport.ac.uk>
2023-02-20 14:05:11 +0000

    implemented apps list buttons and resource list buttons, arranged the search button to be in a container with the search bar so it appears on the side of the search bar

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,11 @@ class MyFlutterState extends State<MyFlutterApp> with TickerProviderStateMixin {
 
   //WRITE VARIABLES AND EVENT HANDLERS HERE
   String applicationResourcesText = "";
+  bool showPopUpExitButton = false;
+  bool appNamePopUp = false;
+  bool resourceNamePopUp = false;
+  List<String> appNames = [];
+  List<String> resourceNames = [];
   List<Resource>? ListOfResources = [];
   Color backgroundColour = Color(0xff3a21d9);
   final textController = TextEditingController();
@@ -39,6 +44,30 @@ class MyFlutterState extends State<MyFlutterApp> with TickerProviderStateMixin {
     // Clean up the controller when the widget is disposed.
     textController.dispose();
     super.dispose();
+  }
+
+  void showAppsList(){
+    setState(() {
+      appNamePopUp = true;
+      resourceNamePopUp = false;
+      showPopUpExitButton = true;
+    });
+  }
+
+  void closePopUps() {
+    setState(() {
+      resourceNamePopUp = false;
+      appNamePopUp = false;
+      showPopUpExitButton = false;
+    });
+  }
+
+  void showResourceList(){
+    setState(() {
+      resourceNamePopUp = true;
+      showPopUpExitButton = true;
+      appNamePopUp = false;
+    });
   }
 
   void sortByConsumedQuantity(){
@@ -74,28 +103,72 @@ class MyFlutterState extends State<MyFlutterApp> with TickerProviderStateMixin {
     });
   }
 
-  void displayAppsAlphabetically(){
-    List<String> listOfNames = back_end().applicationList;
-    listOfNames.sort();
-    String box = '';
-    for(String name in listOfNames){
-      box = '$box$name\n';
-    }
-    setState((){
-      applicationResourcesText = box!;
-    });
+  void getResourcesAndApps() async {
+    resourceNames = (await back_end().getResourceNames()).toList().cast<String>();
+
   }
 
-  void displayResourcesAlphabetically() async{
-    List<dynamic> listOfNames = await back_end().getResourceNames();
-    listOfNames.sort();
-    String box = '';
-    for(String name in listOfNames){
-      box = '$box$name\n';
+  List<Widget> _createResourceButtons(String appsOrResources) { // add functional
+    getResourcesAndApps();
+    List<Widget> buttons = [];
+    if (appsOrResources == 'resources') {
+      resourceNames.sort();
+      for (String name in resourceNames) {
+        if (name != null) {
+          buttons.add(
+            MaterialButton(
+              color: Colors.indigo,
+              minWidth: 200,
+              child: Text(name,
+                style: const TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.normal,
+                    color: Colors.white
+                ),
+              ),
+              onPressed: () {
+                searchFunction("resource:$name");
+              },
+            ),
+          );
+        } else {
+          print('name not found');
+        }
+      }
+      return buttons;
     }
-    setState((){
-      applicationResourcesText = box!;
-    });
+      return buttons;
+  }
+
+  List<Widget> _createAppButtons(){
+    List<String> listOfNames = back_end().applicationList;
+    listOfNames.sort();
+    List<Widget> buttons = [];
+    listOfNames.sort();
+    for (String name in listOfNames) {
+      if (name != null) {
+        buttons.add(
+          MaterialButton(
+            color: Colors.indigo,
+            minWidth: 200,
+            child: Text(
+              name,
+              style: const TextStyle(
+                fontWeight: FontWeight.w400,
+                fontStyle: FontStyle.normal,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () {
+              searchFunction("application:$name");
+            },
+          ),
+        );
+      } else {
+        print('name not found');
+      }
+    }
+    return buttons;
   }
 
   void searchFunction(String searchText) async{
@@ -147,6 +220,36 @@ class MyFlutterState extends State<MyFlutterApp> with TickerProviderStateMixin {
                   alignment: Alignment.topLeft,
                   children: [
                     Align(
+                      alignment: Alignment(-0.35,0.7),
+                      child: Visibility( // visibility widget used to hide and show resource list
+                        visible: resourceNamePopUp,
+                        child: SizedBox(
+                          height: MediaQuery.of(context).size.height*0.7,
+                          width: MediaQuery.of(context).size.width*0.2,
+                          child: SingleChildScrollView(
+                            child: Column(
+                                children: _createAppButtons()
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment(-0.35,0.7),
+                      child: Visibility( // visibility widget used to hide and show apps list
+                        visible: appNamePopUp,
+                        child: SizedBox(
+                          height: MediaQuery.of(context).size.height*0.7,
+                          width: MediaQuery.of(context).size.width*0.2,
+                          child: SingleChildScrollView(
+                            child: Column(
+                                children: _createResourceButtons('resources')
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Align(
                       alignment: const Alignment(-0.8, 0.35),
                       child: MaterialButton( // order by date
                         onPressed:(){ sortByDate();},
@@ -174,9 +277,19 @@ class MyFlutterState extends State<MyFlutterApp> with TickerProviderStateMixin {
                       ),
                     ),
                     Align(
+                      alignment: Alignment(-0.45,-0.55),
+                      child: Visibility(
+                        visible: showPopUpExitButton,
+                        child: IconButton(
+                          icon: const Icon(Icons.close),
+                          onPressed: (){closePopUps();} ,
+                        ),
+                      ),
+                    ),
+                    Align(
                       alignment: const Alignment(-0.8, -0.45),
-                      child: MaterialButton( // display app names button
-                        onPressed:(){ displayAppsAlphabetically();},
+                      child: MaterialButton( // display app name buttons
+                        onPressed:(){ showAppsList();},
                         color: const Color(0xfffffdfd),
                         elevation: 0,
                         shape: const RoundedRectangleBorder(
@@ -203,7 +316,7 @@ class MyFlutterState extends State<MyFlutterApp> with TickerProviderStateMixin {
                     Align(
                       alignment: const Alignment(-0.8, -0.25),
                       child: MaterialButton( // display resource names button
-                        onPressed:(){ displayResourcesAlphabetically();},
+                        onPressed:(){ showResourceList();},
                         color: const Color(0xfffffdfd),
                         elevation: 0,
                         shape: const RoundedRectangleBorder(
@@ -255,33 +368,6 @@ class MyFlutterState extends State<MyFlutterApp> with TickerProviderStateMixin {
                       ),
                     ),
                     Align(
-                      alignment: const Alignment(0, -0.65),
-                      child: MaterialButton( // Search Button
-                        onPressed:(){ searchFunction(textController.text);},
-                        color: const Color(0xfffffdfd),
-                        elevation: 0,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.zero,
-                        ),
-                        textColor: const Color(0xff3a21d9),
-                        height: 60,
-                        minWidth: 100,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 8),
-                        child: const Text(
-                          "Search",
-                          overflow: TextOverflow.fade,
-                          maxLines: 2,
-                          softWrap: true,
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                            fontStyle: FontStyle.normal,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Align(
                       alignment: const Alignment(-0.8, -0.05),
                       child: MaterialButton( // Sort by cost button
                         onPressed:(){ sortByCost();},
@@ -310,17 +396,36 @@ class MyFlutterState extends State<MyFlutterApp> with TickerProviderStateMixin {
                     ),
                     Align(
                         alignment: Alignment(0, -0.9),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width*0.8,
-                          child: TextField(
-                            controller: textController,
-                            decoration: const InputDecoration(
-                              filled: true,
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(),
-                              hintText: 'Enter application or resource in the format(application: application)',
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                width: MediaQuery.of(context).size.width * 0.8,
+                                child: TextField( // search bar textField
+                                  controller: textController,
+                                  decoration: const InputDecoration(
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Enter application or resource in the format(application: application, or resource: resource)',
+                                  ),
+                                ),
+                              ),
                             ),
-                          ),
+                            MaterialButton( // serach button attached to the side of the textField
+                              color: Colors.indigo,
+                              height: 60,
+                              child: const Text(
+                                'Search',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.normal,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              onPressed: (){ searchFunction(textController.text);},
+                            ),
+                          ],
                         )
                     ),
                     Align(
